abbc17bc23354ce4c8750bd52dfc3b53f02408d6
Dmitriy Fedotov <dmitriyfedotov@Dmitriys-MacBook-Pro.local>
2022-06-12 13:25:08 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# from fastapi import FastAPI
+#
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+#
+#
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
+
+
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
